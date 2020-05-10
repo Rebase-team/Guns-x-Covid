6a0b742b2bc7e964ce84e119671930461cd78271
Savio Santos <saviosa08@gmail.com>
2020-05-10 11:19:49 -0300

    Criado alerta para informar se usuario pode responder ou nao, de acordo com sua localizacao de ip.

--- garanhuns-x-covid/src/app/home/home.page.ts
+++ garanhuns-x-covid/src/app/home/home.page.ts
@@ -19,55 +19,53 @@ export class HomePage implements OnInit{
   }
 
   ngOnInit(){
-    this.isGaranhuns();
   }
 
   isGaranhuns(){
+    let result: boolean;
     this.http.get("https://api.ipify.org?format=json").subscribe((ip) => {
-      console.log(Object(ip).ip);
       this.http.get("http://ip-api.com/json/" + Object(ip).ip).subscribe((ipInfo) => {
-        console.log({ latitude: -8.9365336, longitude: -36.6418746}); //Centro de Garanhuns
-        console.log({ latitude: Object(ipInfo).lat, longitude: Object(ipInfo).lon }); //Coordenadas do usuário)
         let isInCity = geolib.isPointWithinRadius(
           { latitude: -8.9365336, longitude: -36.6418746}, //Centro de Garanhuns
           { latitude: Object(ipInfo).lat, longitude: Object(ipInfo).lon }, //Coordenadas do usuário
           7000 //7 km
         );
-        if (isInCity == true){
-          console.log("Está em Garanhuns.");
-        }
-        else{
-          console.log("Não está em Garanhuns.");
-        }
+        result = isInCity;
       });
     });
+    return result;
   }
 
   btnAnswerQuestion() {
-    this.disabledAnswer = true;
-    this.storage.get("allowedTimeToRespond").then((allowedTime) => {
-      if (this.date.getDay() == 0) {
-        this.alertInfo("Domingo", "O App só funciona de segunda à sábado.", "Entendi");
-      }
-      else if (this.date.getHours() < 6 && this.date.getHours() > 18) {
-        this.alertInfo("Hora sem atividade", "O App só funciona das 06 da manhã às 18 da noite.", "Entendi");
-      }
-      else if ((this.isFirstAccess(allowedTime)) || (this.date.toLocaleString() > allowedTime)) {
-        //É bom que horas permitidas venham do servidor
-        let currentDate = new Date(this.date.toLocaleString());
-        //Soma 1 hora a hora atual.
-        currentDate.setHours(currentDate.getHours() + 1).toLocaleString();
-        //Manda a hora somada pro local storage.
-        this.storage.set("allowedTimeToRespond", currentDate);
-        this.alertInfo("Obrigado", "Continue interagingo com o App sempre que possível " +
-          "para contribuir com o bem-estar da cidade." +
-          `<br><strong>Daqui a uma hora você pode informar novamente ` +
-          `como anda a movimentação no centro.</strong>`, "Entendi")
-      }
-      else {
-        this.alertInfo("Já respondeu", "Daqui a uma hora você pode responder novamente.", "Entendi");
-      }
-    });
+    if (this.isGaranhuns() == true){
+      this.disabledAnswer = true;
+      this.storage.get("allowedTimeToRespond").then((allowedTime) => {
+        if (this.date.getDay() == 0) {
+          this.alertInfo("Domingo", "O App só funciona de segunda à sábado.", "Entendi");
+        }
+        else if (this.date.getHours() < 6 && this.date.getHours() > 18) {
+          this.alertInfo("Hora sem atividade", "O App só funciona das 06 da manhã às 18 da noite.", "Entendi");
+        }
+        else if ((this.isFirstAccess(allowedTime)) || (this.date.toLocaleString() > allowedTime)) {
+          //É bom que horas permitidas venham do servidor
+          let currentDate = new Date(this.date.toLocaleString());
+          //Soma 1 hora a hora atual.
+          currentDate.setHours(currentDate.getHours() + 1).toLocaleString();
+          //Manda a hora somada pro local storage.
+          this.storage.set("allowedTimeToRespond", currentDate);
+          this.alertInfo("Obrigado", "Continue interagingo com o App sempre que possível " +
+            "para contribuir com o bem-estar da cidade." +
+            `<br><strong>Daqui a uma hora você pode informar novamente ` +
+            `como anda a movimentação no centro.</strong>`, "Entendi")
+        }
+        else {
+          this.alertInfo("Já respondeu", "Daqui a uma hora você pode responder novamente.", "Entendi");
+        }
+      });
+    }
+    else{
+      this.alertInfo("Não está na cidade", "Você precisa está em um raio de 7 km para poder responder.", "Entendi");
+    }
   }
 
   isFirstAccess(value) {
