b934232b3ea7cc0be6b0706a6f8c3bd25fca5caf
Savio Santos <saviosa08@gmail.com>
2020-05-10 11:11:12 -0300

    Adicionada logica de verificar se usuario esta em Garanhuns.

--- garanhuns-x-covid/src/app/home/home.page.ts
+++ garanhuns-x-covid/src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { AlertController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
+const geolib = require('geolib');
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -24,33 +26,43 @@ export class HomePage implements OnInit{
     this.http.get("https://api.ipify.org?format=json").subscribe((ip) => {
       console.log(Object(ip).ip);
       this.http.get("http://ip-api.com/json/" + Object(ip).ip).subscribe((ipInfo) => {
-        console.log(Object(ipInfo).city);
+        console.log({ latitude: -8.9365336, longitude: -36.6418746}); //Centro de Garanhuns
+        console.log({ latitude: Object(ipInfo).lat, longitude: Object(ipInfo).lon }); //Coordenadas do usuário)
+        let isInCity = geolib.isPointWithinRadius(
+          { latitude: -8.9365336, longitude: -36.6418746}, //Centro de Garanhuns
+          { latitude: Object(ipInfo).lat, longitude: Object(ipInfo).lon }, //Coordenadas do usuário
+          7000 //7 km
+        );
+        if (isInCity == true){
+          console.log("Está em Garanhuns.");
+        }
+        else{
+          console.log("Não está em Garanhuns.");
+        }
       });
     });
   }
 
-  btnAnswerQuestion(){
+  btnAnswerQuestion() {
     this.disabledAnswer = true;
     this.storage.get("allowedTimeToRespond").then((allowedTime) => {
-      if ((this.isFirstAccess(allowedTime)) || (this.date.toLocaleString() > allowedTime)) {
-        if (this.date.getDay() == 7) {
-          this.alertInfo("Domingo", "O App só funciona de segunda à sábado.", "Entendi");
-        }
+      if (this.date.getDay() == 0) {
+        this.alertInfo("Domingo", "O App só funciona de segunda à sábado.", "Entendi");
+      }
+      else if (this.date.getHours() < 6 && this.date.getHours() > 18) {
+        this.alertInfo("Hora sem atividade", "O App só funciona das 06 da manhã às 18 da noite.", "Entendi");
+      }
+      else if ((this.isFirstAccess(allowedTime)) || (this.date.toLocaleString() > allowedTime)) {
         //É bom que horas permitidas venham do servidor
-        else if (this.date.getHours() < 6 && this.date.getHours() > 18){
-          this.alertInfo("Hora sem atividade", "O App só funciona das 06 da manhã às 18 da noite.", "Entendi");
-        }
-        else {
-          let currentDate = new Date(this.date.toLocaleString());
-          //Soma 1 hora a hora atual.
-          currentDate.setHours(currentDate.getHours() + 1).toLocaleString();
-          //Manda a hora somada pro local storage.
-          this.storage.set("allowedTimeToRespond", currentDate);
-          this.alertInfo("Obrigado", "Continue interagingo com o App sempre que possível " +
-                                     "para contribuir com o bem-estar da cidade." +
-                                     `<br><strong>Daqui a uma hora você pode informar novamente ` + 
-                                     `como anda a movimentação no centro.</strong>`, "Entendi")
-        }
+        let currentDate = new Date(this.date.toLocaleString());
+        //Soma 1 hora a hora atual.
+        currentDate.setHours(currentDate.getHours() + 1).toLocaleString();
+        //Manda a hora somada pro local storage.
+        this.storage.set("allowedTimeToRespond", currentDate);
+        this.alertInfo("Obrigado", "Continue interagingo com o App sempre que possível " +
+          "para contribuir com o bem-estar da cidade." +
+          `<br><strong>Daqui a uma hora você pode informar novamente ` +
+          `como anda a movimentação no centro.</strong>`, "Entendi")
       }
       else {
         this.alertInfo("Já respondeu", "Daqui a uma hora você pode responder novamente.", "Entendi");
@@ -58,11 +70,11 @@ export class HomePage implements OnInit{
     });
   }
 
-  isFirstAccess(value){
+  isFirstAccess(value) {
     return (value == null || value == undefined || value == "")
   }
 
-  async alertInfo(title, message, buttonText){
+  async alertInfo(title, message, buttonText) {
     const alert = await this.alert.create({
       header: title,
       message: message,
