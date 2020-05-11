f4792c5019063f67df0bbc364d7c9e9b200c4fe0
Savio Santos <saviosa08@gmail.com>
2020-05-11 14:51:15 -0300

    Adicionado 2 bibs.

--- garanhuns-x-covid/src/app/welcome/welcome.page.ts
+++ garanhuns-x-covid/src/app/welcome/welcome.page.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  constructor(private device: Device) { }
 
   ngOnInit() {
+    console.log(this.device.uuid);
   }
 
 }
