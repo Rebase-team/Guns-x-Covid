3707516d0d2c6e596b63a92457d5c42192ceeb2e
Savio Santos <saviosa08@gmail.com>
2020-05-17 09:11:25 -0300

    Chamada API de votar.

--- garanhuns-x-covid/src/app/home/home.page.ts
+++ garanhuns-x-covid/src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 import { Geolocation } from "@ionic-native/geolocation/ngx";
@@ -8,6 +7,7 @@ import { switchMap, materialize, dematerialize } from 'rxjs/operators';
 import { GunsCovidEvents, CovidApiService, GunsCovidResponses } from '../services/covid-api.service';
 import { InfoCrowdingService } from '../services/info-crowding.service';
 import { Storage } from "@ionic/storage";
+import { AlertService } from '../services/alert.service';
 
 const geolib = require('geolib');
 
@@ -18,7 +18,8 @@ const geolib = require('geolib');
 })
 
 export class HomePage{
-  average = {
+  vote: string = "";
+  average: Object = {
     current: {
       msg: "~",
       color: ""
@@ -26,13 +27,12 @@ export class HomePage{
     min: "~",
     max: "~"
   };
-  source = interval(5000);
+  source: any = interval(5000);
   pauser = new Subject();
   disabledAnswer: boolean = false;
-  locationCoords: any;
   dataNotCollected: boolean = true;
 
-  constructor(private alert: AlertController,
+  constructor(private alert: AlertService,
               private androidPermissions: AndroidPermissions,
               private locationAccuracy: LocationAccuracy,
               private geolocation: Geolocation,
@@ -59,101 +59,7 @@ export class HomePage{
     this.pauser.next(true);
   }
 
-  //Checa permissão do GPS
-  checkGPSPermission() {
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then(
-      result => {
-        if (result.hasPermission) {
-          this.askToTurnOnGPS();
-        } else {
-          this.requestGPSPermission();
-        }
-      },
-      (error) => {
-        this.alertInfo("Problema com o cordova", "Erro ao acessar plugin.", "Entendi");
-      }
-    );
-  }
-
-  //Solicita permissão do GPS
-  private requestGPSPermission() {
-    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
-      if (canRequest) {
-        console.log("4");
-      } else {
-        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
-          .then(
-            () => {
-              this.askToTurnOnGPS();
-            },
-            (error) => {
-              this.alertInfo("Problema com a permissão", "Erro ao solicitar permissão de GPS.", "Entendi");
-            }
-          );
-      }
-    });
-  }
-
-  //Pede para ativar GPS
-  private askToTurnOnGPS() {
-    this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
-      () => {
-        this.getLocationCoordinates();
-      },
-      (error) => {
-        this.alertInfo("Problema com a permissão", "Erro ao tentar pegar permissão de GPS.", "Entendi");
-      }
-    );
-  }
-
-  //Pega localização
-  getLocationCoordinates() {
-    this.disabledAnswer = true;
-    this.geolocation.getCurrentPosition({timeout: 3000}).then((response) => {
-      let isInCity: boolean = geolib.isPointWithinRadius(
-        { latitude: -8.891052, longitude: -36.494519 },
-        { latitude: response.coords.latitude, longitude: response.coords.longitude },
-        5000);
-        
-      /*let preciseDistance = geolib.getPreciseDistance(
-          { latitude: -8.891052, longitude: -36.494519 },
-          { latitude: response.coords.latitude, longitude: response.coords.longitude },
-      );  
-      console.log(preciseDistance);*/
-      // ^^^^^^
-      //Calcula e mostra a distância entre os pontos.
-
-      if (isInCity) {
-        this.alertInfo("Obrigado", "Continue interagindo com o App sempre que possível " +
-          "para contribuir com o bem-estar da cidade." +
-          `<br><strong>Daqui a uma hora você pode informar novamente ` +
-          `como anda a movimentação no centro.</strong>`, "Entendi");
-      }
-      else {
-        this.disabledAnswer = false;
-        this.alertInfo("Longe do centro", "Você precisa está em um raio de 5 km para poder responder.", "Entendi");
-      }
-    }).catch(() => {
-      this.disabledAnswer = false;
-      this.alertInfo('Erro na localização', "Ocorreu uma falha ao tentar pegar sua localização. Verifique sua conexão com à Internet.", "Entendi");
-    });
-  }
-
-  //Alerta sobre a informação do envio da pergunta
-  private async alertInfo(title, message, buttonText) {
-    const alert = await this.alert.create({
-      header: title,
-      message: message,
-      buttons: [
-        {
-          text: buttonText
-        }
-      ]
-    });
-    alert.present();
-  }
-
-  //Balanço de algomeração no dia
+  //Balanço de aglomeração no dia
   private showCasesTodayGuns(){
     let event = new GunsCovidEvents();
     event.OnCasesTodayGaranhuns = (data) => {
@@ -192,6 +98,125 @@ export class HomePage{
     }
     this.storage.get("uuid").then((uuid) => {
       this.covidApi.casesTodayGaranhuns(event,uuid);
+    });
+  }
+
+  //Checa permissão do GPS
+  checkGPSPermission() {
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then(
+      result => {
+        if (result.hasPermission) {
+          this.askToTurnOnGPS();
+        } else {
+          this.requestGPSPermission();
+        }
+      },
+      (error) => {
+        this.alert.activeAlert("Problema com o cordova", "Erro ao acessar plugin.");
+      }
+    );
+  }
+
+  //Solicita permissão do GPS
+  private requestGPSPermission() {
+    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
+      if (canRequest) {
+        //Já permitiu
+      } else {
+        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
+          .then(
+            () => {
+              this.askToTurnOnGPS();
+            },
+            (error) => {
+              this.alert.activeAlert("Problema com a permissão", "Erro ao solicitar permissão de GPS.");
+            }
+          );
+      }
+    });
+  }
+
+  //Pede para ativar GPS
+  private askToTurnOnGPS() {
+    this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
+      () => {
+        this.getLocationCoordinates();
+      },
+      (error) => {
+        this.alert.activeAlert("Problema com a permissão", "Erro ao tentar pegar permissão de GPS.");
+      }
+    );
+  }
+
+  //Pega localização
+  private getLocationCoordinates() {
+    this.geolocation.getCurrentPosition({timeout: 3000}).then((response) => {
+      let isInCity: boolean = geolib.isPointWithinRadius(
+        { latitude: -8.891052, longitude: -36.494519 },
+        { latitude: response.coords.latitude, longitude: response.coords.longitude },
+        5000);
+        
+      /*let preciseDistance = geolib.getPreciseDistance(
+          { latitude: -8.891052, longitude: -36.494519 },
+          { latitude: response.coords.latitude, longitude: response.coords.longitude },
+      );  
+      console.log(preciseDistance);*/
+      // ^^^^^^
+      //Calcula e mostra a distância entre os pontos.
+
+      if (isInCity) {
+        this.submitVote();
+      }
+      else {
+        this.alert.activeAlert("Longe do centro", "Você precisa está em um raio de 5 km para poder responder.");
+      }
+    }).catch(() => {
+      this.disabledAnswer = false;
+      this.alert.activeAlert('Tente novamente', "Ocorreu uma falha ao tentar pegar sua localização. Verifique sua conexão com à Internet.");
+    });
+  }
+
+  private submitVote(){
+    this.disabledAnswer = true;
+    let event = new GunsCovidEvents();
+    event.OnSubmiteVote = (data) => {
+      let dataJSON = JSON.parse(data.data);
+      switch(dataJSON.response){
+        case GunsCovidResponses.SUBMIT_VOTE.VOTE_SUBMITED:
+          this.alert.activeAlert("Obrigado", "Continue interagindo com o App sempre que possível " +
+          "para contribuir com o bem-estar da cidade." +
+          `<br><strong>Daqui a uma hora você pode informar novamente ` +
+          `como anda a movimentação no centro.</strong>`);
+          break;
+        case GunsCovidResponses.SUBMIT_VOTE.ERROR_WHEN_VOTING:
+          this.alert.activeAlert("Erro ao responder", "Tente responder novamente.");
+          break;
+        case GunsCovidResponses.SUBMIT_VOTE.TOO_MANY_VOTES:
+          this.alert.activeAlert("Você já respondeu", "Daqui uma hora a partir da última vez que você respondeu você pode responder novamente.");
+          break;
+        case GunsCovidResponses.SUBMIT_VOTE.UUID_FAILED:
+          this.disabledAnswer = false;
+          console.log("UUID falhou.");
+          break;
+        case GunsCovidResponses.SUBMIT_VOTE.UUID_INVALID:
+          this.disabledAnswer = false;
+          console.log("UUID inválido.");
+          break;
+        case GunsCovidResponses.SUBMIT_VOTE.VOTE_INVALID:
+          this.disabledAnswer = false;
+          console.log("Voto inválido.");
+          break;
+        default:
+          this.disabledAnswer = false;
+          console.log("Erro inesperado.");
+      }
+    }
+    event.OnErrorTriggered = (error) => {
+      this.disabledAnswer = false;
+      console.log(error);
+    }
+    this.storage.get("uuid").then((uuid) => {
+      this.covidApi.submitVote(event, uuid, this.vote);
     })
   }
 }
