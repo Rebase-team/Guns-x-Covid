613fdc25d0abc9a3663a179aaf92659114daf2c2
Muryllo Pimenta <muryllopimenta@gmail.com>
2020-05-26 15:42:18 -0300

    Corrigido bug no back button.

--- garanhuns-x-covid/src/app/tabs/tabs.page.ts
+++ garanhuns-x-covid/src/app/tabs/tabs.page.ts
@@ -9,6 +9,7 @@ import { Platform, NavController, AlertController } from '@ionic/angular';
 export class TabsPage implements OnInit{
   subscribeBackButton: any;
   mutex: Boolean = false;
+  mutexExit: Boolean = false;
   
   constructor(private platform: Platform, private navCtrl: NavController, private alert: AlertController) {}
 
@@ -17,19 +18,23 @@ export class TabsPage implements OnInit{
       if (!this.mutex){
         let url = this.platform.url().toUpperCase();
         if ((url.indexOf('HOME') != -1) || (url.indexOf('SLIDE') != -1)){
-          this.alert.create({
-            header: "Confirmação de saída do App",
-            message: "Deseja sair do App?",
-            buttons: [
-              { text: "Agora não", handler: () => {} }, 
-              { text: 'Sim', handler: () => {
-                  try {
-                    navigator['app'].exitApp();
-                  } catch (e) { }
+          if (!this.mutexExit){
+            this.alert.create({
+              header: "Confirmação de saída do App",
+              message: "Deseja sair do App?",
+              buttons: [
+                { text: "Agora não", handler: () => { this.mutexExit = false; } },
+                {
+                  text: 'Sim', handler: () => {
+                    try {
+                      navigator['app'].exitApp();
+                    } catch (e) { }
+                  }
                 }
-              }
-            ]
-          }).then((alertExit) => { alertExit.present(); });
+              ]
+            }).then((alertExit) => { alertExit.present(); });
+          }
+          
         }
         else{
           this.navCtrl.back();
